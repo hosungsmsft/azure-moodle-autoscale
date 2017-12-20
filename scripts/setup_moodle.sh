7d959136e59984ce7b0affe3240092772cee65e1
Joel May <joel@catalyst-au.net>
2017-12-20 11:43:04 +1100

    Adding fpm config to frontends

--- scripts/setup_moodle.sh
+++ scripts/setup_moodle.sh
@@ -233,6 +233,24 @@ EOF
    # restart Nginx
    sudo service nginx restart 
 
+   # fpm config - overload this 
+   cat <<EOF > /etc/php/7.0/fpm/pool.d/www.conf
+[www]
+user = www-data
+group = www-data
+listen = /run/php/php7.0-fpm.sock
+listen.owner = www-data
+listen.group = www-data
+pm = dynamic
+pm.max_children = 200 
+pm.start_servers = 20 
+pm.min_spare_servers = 22 
+pm.max_spare_servers = 30 
+EOF
+
+   # Restart fpm
+   service php7.0-fpm stop
+
    # Configure varnish startup for 16.04
    VARNISHSTART="ExecStart=\/usr\/sbin\/varnishd -j unix,user=vcache -F -a :80 -T localhost:6082 -f \/etc\/varnish\/moodle.vcl -S \/etc\/varnish\/secret -s malloc,10240m -p thread_pool_min=200 -p thread_pool_max=4000 -p thread_pool_add_delay=2 -p timeout_linger=100 -p timeout_idle=30 -p send_timeout=1800 -p thread_pools=4 -p http_max_hdr=512 -p workspace_backend=512k"
    sed -i "s/^ExecStart.*/${VARNISHSTART}/" /lib/systemd/system/varnish.service
